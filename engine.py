da1317616e9f8566850f8b15aa714a0ae4740209
ren.meta@gmail.com <ren.meta@gmail.com>
2020-06-04 23:24:22 -0400

    updating engine.py to handle input_handlers

--- engine.py
+++ engine.py
@@ -1,26 +1,47 @@
 import tcod as libtcod
 
+from input_handlers import handle_keys
+
 def main():
     screen_width = 80
     screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
 
     # we’re telling libtcod which file and font to use
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     # creates the screen - width/height, title, fullscreen False
     libtcod.console_init_root(screen_width, screen_height, 'rougelike', False)
+    # these variables will hold our keyboard and mouse input
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
+        # captures new 'events' (user input). Updates the key and mouse variables with what the user inputs
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
         # set the color for our ‘@’ symbol, 0 is the console we're drawing to
         libtcod.console_set_default_foreground(0, libtcod.white)
-        # 0 is the console we are printing to, 1, 1 is x, y, coords
-        libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
+        # 0 is the console we are printing to, x, y coords
+        libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
         # presents it to the screen
         libtcod.console_flush()
-        # gets any keyboard input to the program, which we store in the key variable
-        key = libtcod.console_check_for_keypress()
+        # We’re capturing the return value of handle_keys in the variable action (which is a dict)
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            player_x += dx
+            player_y += dy
         # check if the key pressed was the Esc, exit
-        if key.vk == libtcod.KEY_ESCAPE:
+        if exit:
             return True
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
 
 if __name__ == '__main__':
